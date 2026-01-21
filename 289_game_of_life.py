1846c1cad9262ff9500f0767c847402a014830b3
edwimbodzole <80550291+edwimbodzole@users.noreply.github.com>
2026-01-21 16:03:40 +0200

    Create 289_game_of_life.py
    According to Wikipedia's article: "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
    
    The board is made up of an m x n grid of cells, where each cell has an initial state: live (represented by a 1) or dead (represented by a 0). Each cell interacts with its eight neighbors (horizontal, vertical, diagonal) using the following four rules (taken from the above Wikipedia article):
    
    Any live cell with fewer than two live neighbors dies as if caused by under-population.
    Any live cell with two or three live neighbors lives on to the next generation.
    Any live cell with more than three live neighbors dies, as if by over-population.
    Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
    The next state of the board is determined by applying the above rules simultaneously to every cell in the current state of the m x n grid board. In this process, births and deaths occur simultaneously.
    
    Given the current state of the board, update the board to reflect its next state.
    
    O(1) Space

---
+++ 289_game_of_life.py
@@ -0,0 +1,40 @@
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        #Truth table
+        # Original  | New | State
+        #     0     |  0  |   0
+        #     1     |  0  |   1
+        #     0     |  1  |   2
+        #     1     |  1  |   3
+
+        ROWS, COLS = len(board), len(board[0])
+
+        def countNei(row, col):
+            nei = 0
+            for i in range(row - 1, row + 2):
+                for j in range(col - 1, col + 2):
+                    if ((i == row and j == col) or i < 0 or j < 0 or i == ROWS or j == COLS):
+                        continue
+                    if board[i][j] in [1, 3]:
+                        nei += 1
+            return nei
+
+        for row in range(ROWS):
+            for col in range(COLS):
+                nei = countNei(row, col)
+
+                if board[row][col]:
+                    if nei in [2, 3]:
+                        board[row][col] = 3
+                elif nei == 3:
+                    board[row][col] = 2
+
+        for row in range(ROWS):
+            for col in range(COLS):
+                if board[row][col] == 1:
+                    board[row][col] = 0
+                elif board[row][col] in [2, 3]:
+                    board[row][col] = 1
